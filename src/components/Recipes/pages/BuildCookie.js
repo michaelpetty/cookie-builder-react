d280820c6625eced035d2556b4e31fac32d8e913
mpetty <michael.petty@gmail.com>
2019-06-07 11:03:36 -0700

    add addIngred function and display recipes

--- src/components/Recipes/pages/BuildCookie.js
+++ src/components/Recipes/pages/BuildCookie.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Header, List, Button } from 'semantic-ui-react';
 
 class BuildCookie extends React.Component {
   state = {
-    topIngred: {},
+    topIngred: [],
+    recipeResults: [],
+    chosenIngred: []
   }
 
   componentDidMount() {
@@ -19,23 +21,46 @@ class BuildCookie extends React.Component {
       .catch(err => console.log(err));
   }
 
+  addIngred = (id) => {
+    console.log(`clicked a button: ${id}`);
+    axios({
+      method: 'get',
+      url: `http://localhost:4000/api/v1/recipes?ingredient=${id}`
+    })
+      .then(response => {
+        console.log(response);
+        this.setState({ recipeResults: response.data });
+      })
+      .catch(err => console.log(err));
+
+  }
+
+  displayRecipes = recipes => {
+    return recipes.map((recipe, i) => (<List.Item  key={i}>{recipe.Recipe.name}</List.Item>))
+  }
+
   displayTopIngs = ings => {
-    return ings.map((ing, i) => (<List.Item  key={i}> {ing.name}</List.Item>))
+    return ings.map((ing, i) => (<Button size='medium' content={ing.name} onClick={() => this.addIngred(ing.id)} key={i}/>))
   }
 
   render() {
-    const { topIngred } = this.state;
+    const { topIngred, recipeResults } = this.state;
     return (
       <>
         <Header as="h2">cookie builder</Header>
-        {(topIngred[0]) &&
+        {(recipeResults[0]) &&
+          <>
+          <Header as="h3">Matching recipes</Header>
           <List>
-            {this.displayTopIngs(topIngred)}
+            {this.displayRecipes(recipeResults)}
           </List>
+          </>
         }
-        </>
-      )
-    }
+
+        {this.displayTopIngs(topIngred)}
+      </>
+    )
+  }
 }
 
 export default BuildCookie;
