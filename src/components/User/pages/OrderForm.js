457985cff3f1e978c38effc848a210999141dd90
michaelpetty <michael.petty@gmail.com>
2023-01-17 12:00:54 -0800

    major package upgrade; refactor to hooks comp if need useParams from router; import semantic ui css

--- src/components/User/pages/OrderForm.js
+++ src/components/User/pages/OrderForm.js
@@ -1,106 +1,98 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import { Button, Form, List, Header } from 'semantic-ui-react';
 
-class OrderForm extends React.Component {
-  date = new Date();
+const OrderForm = ({setHeader, user, openLogin}) => {
+  const {recipeId} = useParams()
 
-  state = {
-    quantity: 1,
-    recipe: null,
-    cost: 0,
-    expectedDelivery: new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 7),
-    orders: null,
-  }
+  const [quantity, setQuantity] = useState(1)
+  const [recipe, setRecipe] = useState(null)
+  const [cost, setCost] = useState(0)
+  const [expectedDelivery, setExpectedDelivery] = useState(new Date())
+  const [orders, setOrders] = useState(null)
 
-  componentDidMount() {
-    this.props.setHeader('Order Form');
+  useEffect(() =>  {
+    setHeader('Order Form');
     axios({
       method: 'get',
-      url: `${process.env.REACT_APP_API}/api/v1/recipes/${this.props.match.params.recipeId}`
+      url: `${process.env.REACT_APP_API}/api/v1/recipes/${recipeId}`
     })
       .then(response => {
-        this.setState({
-          recipe: response.data,
-          cost: response.data.price,
-        });
+        setRecipe(response.data)
+        setCost(response.data.price)
       })
       .catch(err => console.log(err));
-  }
+  }, [recipeId, setHeader])
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (localStorage.token) {
+      const deliveryDate = new Date(expectedDelivery.getFullYear(), expectedDelivery.getMonth(), expectedDelivery.getDate() + 7)
       axios({
         method: 'post',
         url: `${process.env.REACT_APP_API}/auth/user/orders`,
         headers: { authorization: `Bearer ${localStorage.token}` },
         data: {
-          expectedDelivery: this.state.expectedDelivery,
-          quantity: this.state.quantity,
-          paid: this.state.cost,
-          RecipeId: this.state.recipe.id,
-          UserId: this.props.user.id
+          expectedDelivery: deliveryDate,
+          quantity: quantity,
+          paid: cost,
+          RecipeId: recipe.id,
+          UserId: user.id
         }
       })
       .then(response => {
-        this.setState({ orders: response.data });
+        setOrders(response.data)
+        setExpectedDelivery(deliveryDate)
       })
       .catch(err => console.log(err));
     }
   }
 
-  handleInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-      cost: e.target.value * this.state.recipe.price,
-    });
-
+  const handleQuantity = e => {
+    setQuantity(e.target.value)
+    setCost(e.target.value * recipe.price)
   }
 
-  render() {
-    const { quantity, recipe, cost, expectedDelivery, orders } = this.state;
-    const { user, openLogin } = this.props;
-    const disabled = (!user);
-    document.title = 'Order Form - MP\'s Cookie Factory' ;
+  const disabled = (!user);
+  document.title = 'Order Form - MP\'s Cookie Factory' ;
 
-    return (
+  return (
+    <div>
+      {(disabled) &&
+        <div>Please <Button content='login' onClick={openLogin} compact /> to order</div>
+      }
+      {!(orders) ? (
+        <>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group inline>
+              <Form.Input width={2} fluid id='quantity' name='quantity' type='number' min='1' max='10' value={quantity} onChange={handleQuantity} /> dozen {(recipe) && <>{recipe.name}</>} cookies for ${cost}
+            </Form.Group>
+            <Form.Button content='Place your order' disabled={disabled} />
+          </Form>
+          {(user) &&
+            <>
+              <Header as="h3">Shipping to:</Header>
+              <List>
+                <List.Item>{user.name}</List.Item>
+                <List.Item>{user.street1}</List.Item>
+                {(user.street2 )&& <List.Item>{user.street2}</List.Item> }
+                <List.Item>{user.city}, {user.state}</List.Item>
+                <List.Item>{user.postalCode}</List.Item>
+                </List>
+            </>
+          }
+      </>
+    ) : (
+
       <div>
-        {(disabled) &&
-          <div>Please <Button content='login' onClick={openLogin} compact /> to order</div>
-        }
-        {!(orders) ? (
-          <>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group inline>
-                <Form.Input width={2} fluid id='quantity' name='quantity' type='number' min='1' max='10' value={quantity} onChange={this.handleInput} /> dozen {(recipe) && <>{recipe.name}</>} cookies for ${cost}
-              </Form.Group>
-              <Form.Button content='Place your order' disabled={disabled} />
-            </Form>
-            {(user) &&
-              <>
-                <Header as="h3">Shipping to:</Header>
-                <List>
-                  <List.Item>{user.name}</List.Item>
-                  <List.Item>{user.street1}</List.Item>
-                  {(user.street2 )&& <List.Item>{user.street2}</List.Item> }
-                  <List.Item>{user.city}, {user.state}</List.Item>
-                  <List.Item>{user.postalCode}</List.Item>
-                  </List>
-              </>
-            }
-        </>
-      ) : (
+      Thanks for the order of  dozen {(recipe) && <>{recipe.name}</>} cookies! You can expect delivery by {expectedDelivery.toLocaleDateString()}.
 
-        <div>
-        Thanks for the order of  dozen {(recipe) && <>{recipe.name}</>} cookies! You can expect delivery by {expectedDelivery.toLocaleDateString()}.
-
-        Order #{orders.id}
-        </div>
-      )}
+      Order #{orders.id}
       </div>
-    )
-  }
+    )}
+    </div>
+  )
 }
 
 export default OrderForm;
